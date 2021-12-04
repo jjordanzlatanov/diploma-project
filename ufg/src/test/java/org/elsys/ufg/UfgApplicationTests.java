3190ca8a474cc69a216b7e3d76ef2bd9284f9f4a
jjordanzlatanov <jjordanzlatanov@gmail.com>
2021-12-05 01:18:19 +0200

    [Feat] Migrate to Spring Boot MVC (#8)

---
+++ ufg/src/test/java/org/elsys/ufg/UfgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elsys.ufg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
