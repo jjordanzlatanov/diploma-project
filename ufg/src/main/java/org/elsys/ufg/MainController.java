81b6e0551800a57c9df21803178b47be23cabd8a
jjordanzlatanov <jjordanzlatanov@gmail.com>
2021-12-10 18:47:44 +0200

    [Feat][Ref] Add home page and refactor css code + update git ignore (#9)

---
+++ ufg/src/main/java/org/elsys/ufg/MainController.java
@@ -0,0 +1,26 @@
+package org.elsys.ufg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
