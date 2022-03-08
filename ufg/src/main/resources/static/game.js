155e645aafee3915d57c0300733ce9b274ab247b
jjordanzlatanov <jjordanzlatanov@gmail.com>
2022-03-09 00:29:41 +0200

    [Feat] Create map and add background (#7) (#26) (#4)

--- ufg/src/main/resources/static/game.js
+++ ufg/src/main/resources/static/game.js
@@ -2,19 +2,29 @@
 
 class GameScene extends Phaser.Scene{
     constructor(){
+        super('game')
+    }
+
+    init(){
 
     }
 
     preload(){
-        
+        this.load.image('grass', '../assets/grass.png')
     }
 
     create(){
+        let width = this.sys.game.config.width
+        let height = this.sys.game.config.height
 
+        for(let x = 0; x < width + 10; x += 39){
+            for(let y = 0; y < height + 10; y += 39){
+                this.add.sprite(x, y, 'grass')
+            }
+        }
     }
 
     update(time, delta){
-
     }
 }
 
@@ -22,7 +32,7 @@ let config = {
     type: Phaser.AUTO,
     width: window.innerWidth,
     height: window.innerHeight,
-    backgroundColor: '#000',
+    backgroundColor: '#000000',
     scene: [GameScene]
 }
 
