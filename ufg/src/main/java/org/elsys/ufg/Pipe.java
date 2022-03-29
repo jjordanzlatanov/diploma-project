9b697b8381d7b20ab1682f3148f4cb183c550a03
jjordanzlatanov <jjordanzlatanov@gmail.com>
2022-03-29 14:53:38 +0300

    [Feat][Ref] Add assets for pipe and refactor extra information (#43)

---
+++ ufg/src/main/java/org/elsys/ufg/Pipe.java
@@ -0,0 +1,7 @@
+package org.elsys.ufg;
+
+public class Pipe extends MapObject {
+    public Pipe(Integer startX, Integer startY, Integer endX, Integer endY, String texture, Integer priority) {
+        super(startX, startY, endX, endY, texture, priority);
+    }
+}
